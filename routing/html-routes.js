4397d00040cfa4aa9e90d60c609b29fffc24b429
rich wilk <richardwilkinson@live.com>
2020-12-12 12:36:02 -0500

    added route folder
    added nodemon, added routing folder with html route coded, added requir to server.js

---
+++ routing/html-routes.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = (app) => {
+    app.get("/home", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    app.get("/reserve", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/reservations.html"));
+    });
+    app.get("/tables", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/tables.html"));
+    });
+}
